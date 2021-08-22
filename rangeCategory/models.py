0b5c445ecc6b93b7cdc4eded1993fd6f73e239f2
Veecee <jethrovictor5@yahoo.com>
2021-08-22 12:49:22 +0100

    another commit

--- rangeCategory/models.py
+++ rangeCategory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from authentication.models import User
+from authentication.models import User
 # from django.contrib .auth.models import r
 from django.conf import settings
 from django.utils import timezone
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 
 class Category(models.Model):
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     category_name = models.CharField(max_length=255)
     description = models.TextField()
     date_added = models.DateTimeField(default=timezone.now)
