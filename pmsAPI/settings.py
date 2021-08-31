a80673a61675979c82d765e7a40affbb1b28de39
Veecee <jethrovictor5@yahoo.com>
2021-08-31 17:03:32 +0100

    emails

--- pmsAPI/settings.py
+++ pmsAPI/settings.py
@@ -188,6 +188,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'posmgr@motiv8.com'
+# DEFAULT_FROM_EMAIL = 'posmgr@motiv8.com'
 
 django_heroku.settings(locals())
