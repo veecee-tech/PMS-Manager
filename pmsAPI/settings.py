47fa3aebb8aeab3499395325af3ce4f5b7a88e67
Veecee <jethrovictor5@yahoo.com>
2021-08-31 17:07:55 +0100

    push

--- pmsAPI/settings.py
+++ pmsAPI/settings.py
@@ -185,9 +185,11 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'jethrovictor68@gmail.com'
+EMAIL_HOST_PASSWORD = 'wilson3718'
 EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'posmgr@motiv8.com'
+DEFAULT_FROM_EMAIL = 'jethrovictor68@gmail.com'
 
 django_heroku.settings(locals())
