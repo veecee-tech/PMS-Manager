d11098063f32c2575c0ffd07b2203e40aa37d174
veecee-tech <jethrovictor5@yahoo.com>
2021-09-21 06:59:49 +0100

    modified category, range, and outlet

--- outlet/models.py
+++ outlet/models.py
@@ -9,6 +9,9 @@ class Outlet(models.Model):
     outlet_name = models.CharField(max_length=200, blank=False, null=False)
     outlet_address = models.TextField()
     outlet_officer = models.CharField(max_length=100, default="officer_name")
+    customer_fee = models.CharField(max_length=200)
+    transfer_fee = models.CharField(max_length=200)
+    withdrawal_fee = models.CharField(max_length=200)
     date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- rangeCategory/models.py
+++ rangeCategory/models.py
@@ -7,15 +7,20 @@ from authentication.models import User
 # Create your models here.
 
 
+class RangeTypeChoices(models.TextChoices):
+    customer_fee = u'customer fee for withdrawal', 'Customer Fee For Withdrawal'
+    withdrawal_fee = u'pos fee for withdrawal', 'Pos Fee For Withdrawal'
+    transfer_fee = u'pos fee for transfer', 'Pos Fee For Transfer'
 
 class Category(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    range_type = models.CharField(choices=RangeTypeChoices.choices, max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return self.name +" : " +self.range_type
 
 
 class ChargeChoices(models.TextChoices):
@@ -24,15 +29,14 @@ class ChargeChoices(models.TextChoices):
 
 
 class Range(models.Model):
-    # charge_type_choices = (('percentage', 'Percentage'), ('quantity', 'Quantity'))
 
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE, null=True)
     min_value = models.IntegerField()
     max_value = models.IntegerField()
-    charge_type = models.CharField(choices=ChargeChoices.choices, default=ChargeChoices.PERCENTAGE, max_length=50)
     charge_amount = models.IntegerField()
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, default=User, null=True)
     created_at = models.DateTimeField(default=timezone.now)
 
-    # def __str__(self):
-    #     # return self.in_category
+    def __str__(self):
+        return "{} : {} - {}".format(self.category, self.min_value, self.max_value) 
+
