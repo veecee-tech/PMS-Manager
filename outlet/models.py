fe44123ce4e2127c8793bf223640d0de9e32c62b
veecee-tech <jethrovictor5@yahoo.com>
2021-09-23 11:50:39 +0100

    modified fields for outlet and range category

--- outlet/models.py
+++ outlet/models.py
@@ -6,12 +6,12 @@ from authentication.models import User
 
 class Outlet(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, default=User)
-    outlet_name = models.CharField(max_length=200, blank=False, null=False)
-    outlet_address = models.TextField()
-    outlet_officer = models.CharField(max_length=100, default="officer_name")
-    customer_fee = models.CharField(max_length=200)
-    transfer_fee = models.CharField(max_length=200)
-    withdrawal_fee = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    address = models.TextField()
+    officer = models.CharField(max_length=100, default="officer_name")
+    range_category_customer_fee = models.CharField(max_length=200)
+    range_category_transfer_fee = models.CharField(max_length=200)
+    range_category_withdrawal_fee = models.CharField(max_length=200)
     date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
